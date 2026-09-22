c8f3fdd8038850087873abd2554b7251f40ac08b
JoseRicardoHolguin <13375@ulsachihuahua.edu.mx>
2026-09-21 21:34:54 -0600

    Primera Prueba puerto 4000

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import pokemonRoutes from './routes/pokemon.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'PokéDex Manager API funcionando' });
+});
+
+app.use('/api/pokemon', pokemonRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
